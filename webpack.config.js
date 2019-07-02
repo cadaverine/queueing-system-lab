58aec70017421f34e9d0bfcdb3bc3a4c67cd3401
cadaverine <kennville@gmail.com>
2019-07-02 23:12:52 +0300

    env: add 'webpack.config.js'

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Queuing system'
+    }),
+    new CopyPlugin([
+      { from: 'src/assets', to: 'assets' },
+    ])
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
