e9eb10569ca2327f8cdccb978864a58d6a452c50
cadaverine <kennville@gmail.com>
2019-07-03 08:32:39 +0300

    feat(Request.js): add 'Request' component

---
+++ src/Request.js
@@ -0,0 +1,88 @@
+import { Graphics, Text } from 'pixi.js';
+
+
+let requestsNumber = 0;
+const allowedTypes = ['XCHG', 'CARD', 'CRED', 'ACNT'];
+
+
+export function getRandomRequestType() {
+  const { length } = allowedTypes;
+
+  const type = allowedTypes[(Math.round(Math.random() * 10) % length)]
+  return type
+}
+
+
+export default class Request extends Graphics {
+  static get number() {
+    return requestsNumber;
+  }
+
+  constructor(options = {}) {
+    const defaultOptions = {
+      x: 500,
+      y: 500,
+      type: 'XCHG',
+    }
+
+    const initialOptions = Object.assign({}, defaultOptions, options);
+
+    super(initialOptions);
+    Graphics.call(this);
+
+    this._styling(initialOptions);
+
+    this.id = requestsNumber;
+    requestsNumber += 1;
+  }
+
+  appendTo(parent) {
+    parent.addChild(this);
+  }
+
+  clear() {
+    this.destroy();
+  }
+
+  _styling(options) {
+    const { x, y, type } = options;
+
+    let backgroundColor;
+    switch (type) {
+      case 'XCHG':
+        backgroundColor = 0x40DC91;
+        break;
+      case 'CARD':
+        backgroundColor = 0x54B7FF;
+        break;
+      case 'CRED':
+        backgroundColor = 0x724EFF;
+        break;
+      case 'ACNT':
+        backgroundColor = 0xFD5D5D;
+        break;
+      default:
+        backgroundColor = 0xFFFFFF;
+    }
+
+    this.lineStyle(2, 0x777777, 10);
+    this.beginFill(backgroundColor);
+    this.drawRoundedRect(20, 20, 100, 50, 10);
+    this.endFill();
+
+    const textStyle = {
+      fontFamily : 'Arial',
+      fontSize: 24,
+      fill : "white",
+      align : 'right'
+    }
+    const text = new Text(type, textStyle);
+    text.position.set(70, 45);
+    text.anchor.set(0.5, 0.5);
+
+    this.addChild(text);
+
+    this.x = x;
+    this.y = y;
+  }
+}
