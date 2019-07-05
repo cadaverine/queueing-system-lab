4c202ed68e1ec7d2d4cd81c79399866876b56be3
cadaverine <kennville@gmail.com>
2019-07-05 07:20:31 +0300

    feat(utils.js): add 'getRandomType'

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -7,3 +7,12 @@ export function range(from, to) {
 
   return arr;
 }
+
+export function getRandomType(types) {
+  const { length } = types;
+
+  const type = types[(Math.round(Math.random() * 10) % length)]
+
+  return type
+}
+
