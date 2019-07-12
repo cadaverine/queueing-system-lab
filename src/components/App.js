86763ba8b85d22f0678a46b93267371f26c7d4b3
cadaverine <kennville@gmail.com>
2019-07-12 09:05:14 +0300

    feat(App.js): add 'ServiceManager'

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import Request from './Request';
 import ServiceDevice from './ServiceDevice';
+import ServiceManager from './ServiceManager';
 import { Application } from 'pixi.js';
 import { range, getRandomType } from '../helpers/utils';
 
@@ -20,28 +21,16 @@ export default class App {
 
     parent.appendChild(this.app.view);
 
-    this.requests = [];
-    this.devicesTypes = ['NO', 'NO', 'NX', 'NX', 'NX', 'NX', 'NX'];
-    this.currentRequest = null;
+    this.manager = new ServiceManager({ scene: this.app.stage });
+    this.services = new Map();
   }
+
+  // createControlPanel() {
+
+  // }
 
 
   setup() {
-    this.requests = range(0, 10).map(i => new Request({
-      x: 400 + 15 * i,
-      y: 280,
-      type: getRandomType(Request.types),
-    }))
-
-    this.devices = this.devicesTypes.map((type, i) => new ServiceDevice({
-      x: 1000,
-      y: 50 + 75 * i,
-      type,
-    }))
-
-    this.devices.forEach(request => request.appendTo(this.app.stage));
-    this.requests.forEach(request => request.appendTo(this.app.stage));
-
     this.ticker = this.app.ticker.add(delta => this.gameLoop(delta));
     this.ticker.stop();
   }
@@ -60,15 +49,23 @@ export default class App {
   gameLoop(delta) {
     if (this.currentRequest != null) {
       if (this.currentRequest.served === true) {
-        this.currentRequest.clear()
+        this.services.delete(this.currentRequest);
+        this.currentRequest.clear();
         this.currentRequest = null;
       } else {
-        // this.moveRequest(this.currentRequest, 3 + delta);
-        this.moveRequestToDevice(this.devices[2], this.currentRequest);
+        if (!this.services.has(this.currentRequest)) {
+          const device = this.manager.getSuitableDevice(this.currentRequest);
+
+          this.services.set(this.currentRequest, device);
+        }
+
+        const device = this.services.get(this.currentRequest);
+
+        this.manager.moveRequestToDevice(this.currentRequest, device);
       }
     } else {
-      if (this.requests.length > 0) {
-        this.currentRequest = this.requests.pop()
+      if (this.manager.requests.length > 0) {
+        this.currentRequest = this.manager.requests.pop()
         this.currentRequest.setVelocity({
           y: 3 + delta,
           x: 3 + delta,
@@ -77,55 +74,8 @@ export default class App {
     }
 
     if (Math.random() > 0.99) {
-      const newRequest = this.createRequest();
-      this.addRequestToQueue(newRequest);
+      const newRequest = this.manager.generateRequest();
+      this.manager.addRequestToQueue(newRequest);
     }
-  }
-
-
-  moveRequestToDevice(device, request) {
-    const slot = device.slotCoords;
-
-    if (slot.x - request.x > 200) {
-      request.moveX();
-    } else if (slot.y !== request.y) {
-      if (Math.abs(slot.y - request.y) < Math.abs(request.vy)) {
-        request.vy = Math.abs(slot.y - request.y);
-      }
-
-      if (slot.y - request.y < 0) {
-        request.y -= request.vy;
-      } else {
-        request.y += request.vy;
-      }
-
-      // request.moveY();
-    } else if (slot.x !== request.x) {
-      if (Math.abs(slot.x - request.x) <= request.vx) {
-        request.vx = slot.x - request.x;
-      }
-
-      request.moveX();
-    } else {
-      request.served = true;
-    }
-  }
-
-
-  createRequest(requestType) {
-    const type = requestType != null ? requestType : getRandomType(Request.types);
-
-    return new Request({ x: 400, y: 280, type });
-  }
-
-
-  addRequestToQueue(request) {
-    this.requests.forEach((request, i) => {
-      request.zIndex += 1;
-      request.x += 15;
-    });
-
-    request.prependTo(this.app.stage);
-    this.requests.unshift(request);
   }
 }
