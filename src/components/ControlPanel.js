fde32bba41ec9e69ff912975ae97dd5c2c3d8fb5
cadaverine <kennville@gmail.com>
2019-10-10 23:44:38 +0300

    feat(ControlPanel.js): add layout, 'state'

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,16 +1,42 @@
 export default class ControlPanel {
   constructor() {
-    this.view = document.createElement('div');
+    this.view = document.createElement('div')
+
     this.view.className = 'control-panel';
+    this.view.innerHTML = `
+      <div class="wrapper-buttons">
+        <button class="button button_start">▶ Start</button>
+        <button class="button button_stop">◾ Stop</button>
+      </div>
+      <div class="wrapper-controls"></div>
+      <div class="wrapper-statistics"></div>
+    `;
 
-    this.startButton = document.createElement('button');
-    this.startButton.textContent = 'Start';
-    this.startButton.className = 'button button_start';
+    [this.startButton] = this.view.getElementsByClassName('button_start');
+    [this.stopButton] = this.view.getElementsByClassName('button_stop');
 
-    this.pauseButton = document.createElement('button');
-    this.pauseButton.textContent = 'Pause';
-    this.pauseButton.className = 'button button_pause';
+    this._state = 'stopped';
+  }
 
-    this.view.append(this.startButton, this.pauseButton);
+
+  get state() {
+    return this._state;
+  }
+
+
+  set state(value) {
+    switch(value) {
+      case 'started':
+        this.startButton.textContent = '▮▮ Pause';
+        break;
+      case 'paused':
+        this.startButton.textContent = '▶ Start';
+        break;
+      case 'stopped':
+        this.startButton.textContent = '▶ Start';
+        break
+    }
+
+    this._state = value;
   }
 }
