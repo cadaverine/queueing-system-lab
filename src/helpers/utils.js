82daaa78f82de6d5f88774214393796e31c8294b
cadaverine <kennville@gmail.com>
2019-10-15 00:08:18 +0300

    fix(utils.js): fix range function

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,7 +1,7 @@
 export function range(from, to) {
   const arr = [];
 
-  for (let i = 0, j = from; j <= to; j = from + ++i) {
+  for (let i = 0, j = from; j < to; j = from + ++i) {
     arr[i] = j;
   }
 
